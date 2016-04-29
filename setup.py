29fbba783625b0c0741bff958ff864d02c28fa45
wtjerry <wtjerry123@gmail.com>
2016-04-29 11:30:31 +0200

    add version and license information

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name="beautifyMusic",
+    version="0.1",
+    license="GPLv2+",
     install_requires=["mutagenx"],
     packages=["beautifyMusic"],
     entry_points={"console_scripts": ["beautifyMusic = beautifyMusic.__main__:main"]}
