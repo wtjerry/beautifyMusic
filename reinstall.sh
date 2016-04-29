92224f7ebe79775259619af72b29dcf42425d9d4
wtjerry <wtjerry123@gmail.com>
2016-04-29 16:49:59 +0200

    change reinstall: inits basedir inside of script

--- reinstall.sh
+++ reinstall.sh
@@ -1,1 +1,4 @@
-pip3 uninstall beautifyMusic; python3 setup.py sdist && pip3 install --user dist/beautifyMusic-0.1.tar.gz
+cd $(dirname $0)
+
+pip3 uninstall beautifyMusic
+python3 setup.py sdist && pip3 install --user dist/beautifyMusic-0.1.tar.gz
