c881675344c9958b28579c78f961a05cbcbc5ab1
wtjerry <wtjerry123@gmail.com>
2016-04-29 15:57:04 +0200

    raise ValueError if not enough arguments AND add new album argument

--- beautifyMusic/beautify_song365.py
+++ beautifyMusic/beautify_song365.py
@@ -1,6 +1,7 @@
 #
 # arg1 path
-# arg2 'dryRun' to not make any changes
+# arg2 album name
+# arg3 'dryRun' to not make any changes
 #
 
 import sys
@@ -55,21 +56,30 @@ class beautify_song365(object):
         id3File.save()
 
 
-  def beautify(self):
-    self.path = sys.argv[1]
-    if (not self.path[-1] == "/"):
-      self.path = self.path + "/"
-    self.isDryRun = len(sys.argv) > 2 and sys.argv[2]=="dryRun"
-    self.ending = ".mp3"
-    self.middlePart = "_(song365.cc)"
-
+  def renameFiles(self):
+    print(self.path + " " + self.ending)
     filePath = glob(self.path + "*" + self.ending)[0]
     f = filePath.split('/')[-1]
     prefix = self.extractArtist(f) + " - "
+    beautify(self.path, self.ending, prefix, self.middlePart, self.isDryRun)
+
+
+  def beautify(self):
+    if len(sys.argv) < 3:
+      raise ValueError("Not enough arguments. Arg1: path, Arg2: album, (Arg3: 'dryRun')")
+
+    self.path = sys.argv[1]
+    if (not self.path[-1] == "/"):
+      self.path = self.path + "/"
+    self.album = sys.argv[2]
+    self.isDryRun = len(sys.argv) > 3 and sys.argv[3]=="dryRun"
+    self.ending = ".mp3"
+    self.middlePart = "_(song365.cc)"
 
     files = self.getOnlyFiles()
-    self.addArtist(files)
-    self.addTitle(files)
-    beautify(self.path, self.ending, prefix, self.middlePart, self.isDryRun)
+    if (len(files) > 0):
+      self.addArtist(files)
+      self.addTitle(files)
+      self.renameFiles()
 
 
